d7dfddd9e2b6b4107e85a18670121252e62512ba
YCPKirstynSwancer <47125827+YCPKirstynSwancer@users.noreply.github.com>
2019-11-06 20:51:05 -0500

    11-6 Cleaning Up Russian Nesting Dolls
    Had a meeting with Professor to clean up code and fix my nesting code.
    Have sections working properly now and need to clean up the rest of the code on the other pages.

--- js/main.js
+++ js/main.js
@@ -9,33 +9,21 @@ function closeNav() {
   document.getElementById("main").style.marginRight= "0";
 }
 //EndSideNav//
-//SmoothScroll//
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script>
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
+// Begin Accordion //
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
   });
-});
-</script>
-
-//EndSmoothScroll//
+}

a7abd943637fe332b0a264bc2dcd01e1baf6b0d0
YCPKirstynSwancer <47125827+YCPKirstynSwancer@users.noreply.github.com>
2019-12-01 19:09:18 -0500

    Changes made through 11-25
    Getting the coding to function properly on the home page header and footer. Also getting the alignment to function correct on the about page, just a few alignment errors still and once resolved moving into becoming mobile friendly.

--- js/hero.js
+++ js/hero.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
 $('.single-item').slick();
 });
+
+
--- js/main.js
+++ js/main.js
@@ -29,22 +29,23 @@ for (i = 0; i < acc.length; i++) {
 }
 
 
-    //Get the button
-    var mybutton = document.getElementById("myBtn");
 
-    // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function() {scrollFunction()};
+//Get the button
+var mybutton = document.getElementById("myBtn");
 
-    function scrollFunction() {
-      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-      } else {
-        mybutton.style.display = "none";
-      }
-    }
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-    // When the user clicks on the button, scroll to the top of the document
-    function topFunction() {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
-    }
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
